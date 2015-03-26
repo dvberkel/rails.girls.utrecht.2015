bc1ea904dbab1292ebc6be3eabf406134268eb4e
dvberkel <daan.v.berkel.1980@gmail.com>
2015-03-26 11:30:28 +0100

    Change heart angle

--- js/application.js
+++ js/application.js
@@ -3,7 +3,7 @@
     'use strict';
 
     Reveal.addEventListener('heart', function(event){
-        var model = new heart.Model(20, 320, 480);
+        var model = new heart.Model(40, 320, 480);
 
         var leftControlPoints = new heart.ControlPoints(model, -1, 320, 480);
         var leftHeart = document.getElementById('left-heart');
