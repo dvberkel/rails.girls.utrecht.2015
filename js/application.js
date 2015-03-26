b4290f71ad322a5c942de5dfa5dc2209b4e2dbf7
dvberkel <daan.v.berkel.1980@gmail.com>
2015-03-26 08:10:06 +0100

    Interact with the heart slide

--- js/application.js
+++ js/application.js
@@ -1,6 +1,18 @@
-/*global Reveal*/
-(function(Reveal){
+/*global Reveal, document, heart, console*/
+(function(Reveal, heart){
+    'use strict';
+
     Reveal.addEventListener('heart', function(event){
-        console.log('visited page ', event.type);
+        var model = new heart.Model(20, 320, 480);
+
+        var leftControlPoints = new heart.ControlPoints(model, -1, 320, 480);
+        var leftHeart = document.getElementById('left-heart');
+
+        new heart.View(leftControlPoints, leftHeart);
+
+        var rightControlPoints = new heart.ControlPoints(model, +1, 320, 480);
+        var rightHeart = document.getElementById('right-heart');
+
+        new heart.View(rightControlPoints, rightHeart);
     });
-})(Reveal);
+})(Reveal, heart);
