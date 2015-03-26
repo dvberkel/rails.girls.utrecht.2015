7e2270cd6011c8ddf4047e91b45d4940d78f841b
dvberkel <daan.v.berkel.1980@gmail.com>
2015-03-26 13:36:25 +0100

    Deregistered handler for animation

--- js/application.js
+++ js/application.js
@@ -1,8 +1,8 @@
-/*global document, Reveal, Velocity, heart, console*/
+/*global document, Reveal, Velocity, heart*/
 (function(Reveal, Velocity, heart){
     'use strict';
 
-    Reveal.addEventListener('heart', function(event){
+    Reveal.addEventListener('heart', function(){
         function paintHearts(color){
             [leftHeart, rightHeart].forEach(function(heart){
                 heart.setAttribute('fill', color);
@@ -24,7 +24,7 @@
         paintHearts('red');
 
         var state = 0;
-        document.body.addEventListener('keydown', function(event){
+        function handler(event){
             if (event.keyCode === 65) {
                 if (state === 0) {
                     Velocity(rightHeart, {translateX: 480}, 2000);
@@ -34,9 +34,11 @@
                 }
                 if (state === 2) {
                     Velocity(rightHeart, {translateX: 0}, 2000);
+                    document.body.removeEventListener('keydown', handler);
                 }
                 state++;
             }
-        });
+        }
+        document.body.addEventListener('keydown', handler);
     });
 })(Reveal, Velocity, heart);
